01dcbe762b457b6241527ac0802e304974514cc5
Bobby <alkaifkhawarizmi@gmail.com>
2025-06-24 10:57:44 +0530

    test

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import React from "react";
-// import { SignupCard } from "../../components/signup-card";
-import { Squares } from "@/components/ui/squares-background";
-import { Navbar } from "@/components/ui/mini-navbar";
 import { SignInPage } from "@/components/blocks/sign-in-flow-1";
 
 function Auth() {
