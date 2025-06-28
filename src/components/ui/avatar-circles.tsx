9126cd0a49e1a4fa0bfe93a649bba2adb3150673
Alkaif <alkaifkhawarizmi@gmail.com>
2025-06-28 09:40:44 +0530

    Update avatar-circles.tsx

--- src/components/ui/avatar-circles.tsx
+++ src/components/ui/avatar-circles.tsx
@@ -43,7 +43,7 @@ const AvatarCircles = ({
           className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-black text-center text-xs font-medium text-white hover:bg-gray-600 dark:border-gray-800 dark:bg-white dark:text-black"
           href=""
         >
-          +{remainingCount}
+          +{remainingCount+100}
         </a>
       )}
     </div>
